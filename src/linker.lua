034f769f8689785d6e80b523b5b9b055613b394c
Yonaba <roland.yonaba@gmail.com>
2014-10-30 10:05:40 +0000

    Added linker

---
+++ src/linker.lua
@@ -0,0 +1,14 @@
+file = io.open("data/options.spl", "r")
+way = file:read()
+file:close()
+
+if way == "GrandPrix" then 
+	file = io.open("data/cup.spl", "r")
+	cup = file:read()
+	file:close()
+	if cup == "cup1" then dofile("cup1_1.lua")
+	elseif cup == "cup2" then dofile("cup2_1.lua")
+	elseif cup == "cup3" then dofile("cup3_1.lua")
+	end
+	elseif way == "TimeTrial" then dofile("c_track.lua")
+end
